ee61133e38c297fa774ef0d36d88b61598b08aa1
Salavat <altinbaevsal@gmail.com>
2021-02-14 18:41:30 +0300

    Ready

--- ex_1.py
+++ ex_1.py
@@ -1,1 +1,7 @@
-print("It's my first exercise")
+import random
+
+
+first_list = [random.randint(-5, 5) for i in range(1, random.randint(5, 15))]
+second_list = [random.randint(-5, 5) for y in range(1, random.randint(5, 15))]
+my_list = set(first_list) - set(second_list)
+print(my_list)
